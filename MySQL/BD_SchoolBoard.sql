27d3165e20bba8ebde35ae510703bf28f7d7cbd4
unknown <yuraniester@gmail.com>
2023-10-05 14:39:11 -0500

    Se agrega observador Admin

--- MySQL/BD_SchoolBoard.sql
+++ MySQL/BD_SchoolBoard.sql
@@ -23,21 +23,26 @@ CREATE TABLE usuario (
     PRIMARY KEY (documento)
 );
 
-INSERT INTO usuario (documento, clave, rol, estado, nombres, apellidos, correo, foto) VALUES 
-(1, MD5('administrador'), 'Administrador', 'activo', 'Administrador', 'Prueba', 'yuraniester@gmail.com', '../../Uploads/Usuario/fotoUsuario.jpg'),
-(2, MD5('docente'), 'Docente', 'activo', 'Docente', 'Prueba', 'yuraniester@gmail.com', '../../Uploads/Usuario/fotoUsuario.jpg'),
-(3, MD5('estudiante'), 'Estudiante', 'activo', 'Estudiante', 'Prueba', 'yuraniester@gmail.com', '../../Uploads/Usuario/fotoUsuario.jpg'),
-(12345, MD5('12345'), 'Docente', 'activo', 'Felipe', 'Restrepo','lfrestrepo004@gmail.com', '../../Uploads/Usuario/fotoUsuario.jpg'),
-(1023163094, MD5('estudiante'), 'Estudiante', 'activo', 'Nicole', 'Benavides', 'yuraniester@gmail.com', '../../Uploads/Usuario/fotoUsuario.jpg');
+INSERT INTO usuario (documento, clave, rol, estado, tipoDoc, nombres, apellidos, correo, foto) VALUES 
+(1, MD5('administrador'), 'Administrador', 'activo', 'CC','Administrador', 'Prueba', NULL, '../../Uploads/Usuario/fotoUsuario.jpg'),
+(2, MD5('docente'), 'Docente', 'activo', 'CC','Docente', 'Prueba', NULL, '../../Uploads/Usuario/fotoUsuario.jpg'),
+(3, MD5('estudiante'), 'Estudiante', 'activo', 'TI','Estudiante', 'Prueba', NULL, '../../Uploads/Usuario/fotoUsuario.jpg'),
+(12345, MD5('12345'), 'Docente', 'activo', 'CC','Felipe', 'Restrepo','lfrestrepo004@gmail.com', '../../Uploads/Usuario/fotoUsuario.jpg'),
+(1023163094, MD5('estudiante'), 'Estudiante', 'activo', 'TI','Nicole', 'Benavides', 'yuraniester@gmail.com', '../../Uploads/Usuario/fotoUsuario.jpg');
 
-CREATE TABLE acudiente (
-    documento INT,
-    nombres VARCHAR(100),
-    apellidos VARCHAR(100),
-    telefono VARCHAR(15),
-    correo VARCHAR(100),
-    PRIMARY KEY (documento)
+CREATE TABLE asignatura (
+    idAsignatura INT AUTO_INCREMENT,
+    nombre VARCHAR(100),
+    PRIMARY KEY (idAsignatura)
 );
+
+INSERT INTO asignatura (nombre) VALUES 
+('Matemáticas'),
+('Español'),
+('Ciencias'),
+('Sociales'),
+('Fisica'),
+('Ciencias prueba');
 
 CREATE TABLE curso (
     idCurso INT AUTO_INCREMENT,
@@ -55,6 +60,70 @@ CREATE TABLE aula (
     idAula INT AUTO_INCREMENT,
     nombre VARCHAR(50),
     PRIMARY KEY (idAula)
+);
+
+INSERT INTO aula(nombre) VALUES 
+('Laboratorio 102'),
+('119 Torre oriental'),
+('301 Torre occidental');
+
+CREATE TABLE observador (
+    idObservador INT AUTO_INCREMENT,
+    idEstudiante INT,
+    idAutor INT,
+    fecha DATETIME DEFAULT NOW(),
+    observacion VARCHAR(400),
+    PRIMARY KEY (idObservador),
+    FOREIGN KEY (idEstudiante) REFERENCES usuario(documento),
+    FOREIGN KEY (idAutor) REFERENCES usuario(documento)
+);
+
+INSERT INTO observador(idEstudiante, idAutor, observacion) VALUES 
+(3, 1,'El estudiante no cumple con el uniforme de la institución'),
+(3, 1,'El estudiante no presta atención en clase y distrae a sus compañeros');
+
+CREATE TABLE comunicado (
+    idComunicado INT AUTO_INCREMENT,
+    idUsuario INT,
+    idCurso INT,
+    titulo VARCHAR(200),
+    fecha DATETIME DEFAULT NOW(),
+    descripcion VARCHAR(400),
+    archivos VARCHAR(200),
+    PRIMARY KEY (idComunicado),
+    FOREIGN KEY (idUsuario) REFERENCES usuario(documento),
+    FOREIGN KEY (idCurso) REFERENCES curso(idCurso)
+);
+
+INSERT INTO comunicado(titulo, descripcion, archivos) VALUES 
+('Cancelación de clase', 'El día de mañana no habrá clase en porque se suspenderá el agua en el colegio','../Vista/Uploads/Comunicados/Arte Marcial - Felipe Angarita.pdf'),
+('Entrega de boletines', 'La entrega de voletines se hará el día 20/11/2024 desde las 8:00a.m. En los proximos días el director de cada uno de los cursos estará comunicando la hora exacta de la reunión para cada estudiante.','../Vista/Uploads/Comunicados/Arte Marcial - Felipe Angarita.pdf');
+
+CREATE TABLE clase (
+    idClase INT AUTO_INCREMENT,
+    idCurso INT,
+    idAsignatura INT,
+    idDocente INT, 
+    idAula INT,
+    descripción VARCHAR(400),
+    PRIMARY KEY (idClase),
+    FOREIGN KEY (idCurso) REFERENCES curso(idCurso),
+    FOREIGN KEY (idDocente) REFERENCES usuario(documento),
+    FOREIGN KEY (idAsignatura) REFERENCES asignatura(idAsignatura)
+);
+
+INSERT INTO clase (idCurso, idAsignatura, idDocente) VALUES
+(1, 2, 2),
+(2, 1, 2);
+
+
+CREATE TABLE acudiente (
+    documento INT,
+    nombres VARCHAR(100),
+    apellidos VARCHAR(100),
+    telefono VARCHAR(15),
+    correo VARCHAR(100),
+    PRIMARY KEY (documento)
 );
 
 CREATE TABLE estudianteAcudiente (
@@ -80,37 +149,6 @@ INSERT INTO estudianteCurso (idCurso, idEstudiante) VALUES
 (1, 1023163094),
 (1, 3);
 
-CREATE TABLE asignatura (
-    idAsignatura INT AUTO_INCREMENT,
-    nombre VARCHAR(100),
-    PRIMARY KEY (idAsignatura)
-);
-
-INSERT INTO asignatura (nombre) VALUES 
-('Matemáticas'),
-('Español'),
-('Ciencias'),
-('Sociales'),
-('Fisica'),
-('Ciencias prueba');
-
-CREATE TABLE clase (
-    idClase INT AUTO_INCREMENT,
-    idCurso INT,
-    idAsignatura INT,
-    idDocente INT, 
-    idAula INT,
-    descripción VARCHAR(400),
-    PRIMARY KEY (idClase),
-    FOREIGN KEY (idCurso) REFERENCES curso(idCurso),
-    FOREIGN KEY (idDocente) REFERENCES usuario(documento),
-    FOREIGN KEY (idAsignatura) REFERENCES asignatura(idAsignatura)
-);
-
-INSERT INTO clase (idCurso, idAsignatura, idDocente) VALUES
-(1, 2, 2),
-(2, 1, 2);
-
 CREATE TABLE asistencia (
     idAsistencia INT AUTO_INCREMENT,
     idClase INT,
@@ -122,29 +160,6 @@ CREATE TABLE asistencia (
     FOREIGN KEY (idEstudiante) REFERENCES usuario(documento)
 );
 
-CREATE TABLE observador (
-    idObservador INT AUTO_INCREMENT,
-    idEstudiante INT,
-    idDocente INT,
-    fecha DATETIME DEFAULT NOW(),
-    observacion VARCHAR(400),
-    PRIMARY KEY (idObservador),
-    FOREIGN KEY (idEstudiante) REFERENCES usuario(documento),
-    FOREIGN KEY (idDocente) REFERENCES usuario(documento)
-);
-
-CREATE TABLE comunicado (
-    idComunicado INT AUTO_INCREMENT,
-    idUsuario INT,
-    idCurso INT,
-    titulo VARCHAR(200),
-    fecha DATETIME DEFAULT NOW(),
-    descripcion VARCHAR(400),
-    archivos VARCHAR(200),
-    PRIMARY KEY (idComunicado),
-    FOREIGN KEY (idUsuario) REFERENCES usuario(documento),
-    FOREIGN KEY (idCurso) REFERENCES curso(idCurso)
-);
 
 CREATE TABLE tarea (
     idTarea INT AUTO_INCREMENT,
@@ -157,7 +172,6 @@ CREATE TABLE tarea (
     PRIMARY KEY(idTarea),
     FOREIGN KEY (idClase) REFERENCES clase(idClase)
 );
-
 
 INSERT INTO tarea (idClase, titulo, descripcion, fecha_creacion, fecha_vencimiento, archivos) VALUES
 (1, 'Ensayo sobre Tecnologia1', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus magnam enim natus explicabo amet beatae saepe iste veniam sed quisquam.', '2023-09-05 13:12:13', '2023-09-13 13:11:05', NULL),
---
+++ Vista/html/Administrador/adminObser.php
@@ -0,0 +1,63 @@
+<?php
+    require_once ('../../../Modelo/conexion.php');
+    require_once ('../../../Modelo/consultas.php');
+    require_once ('../../../Modelo/seguridadAdmin.php');
+    require_once ('../../../Controlador/mostrarPerfil.php');
+    require_once ('../../../Controlador/mostrarObserAdmin.php');
+?>
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../../img/logo.svg">
+    <link rel="stylesheet" type="text/css" href="../../css/estilosBase.css">
+    <link rel="stylesheet" type="text/css" href="../../css/administrador/estilosAdmin.css">
+    <script src="../../js/controlGeneral.js"></script>
+    <title>Observador Admin</title>
+</head>
+<body>
+    
+    <?php
+        include("menu-include.php");
+    ?>
+
+        <main>
+            
+            <!-- breadcrumb -->  
+            <nav class="nav-main">
+                <a href="homeAdmin.php">Home</a>
+                <a href="adminObser.php"> / Observador</a>
+            </nav>
+        
+            <section>
+
+            <?php
+                if(isset($_GET['documento'])){
+                    cargarObservador();
+                }else {
+                    mostrarObserForm();
+                }
+            ?>
+
+            </section>
+    
+        </main>
+
+    </div>
+
+    <hr>
+
+    <footer>
+        <div class="info-footer">
+            <p>School Board</p>
+            <p>Copyright © - 2023. Todos los Derechos Reservados</p>
+            <p>Autor: Estefani Arenas, Erika Diaz, Nicole Benavides y Tatiana Arevalo.</p>
+        </div>
+    </footer>
+    
+</body>
+</html>

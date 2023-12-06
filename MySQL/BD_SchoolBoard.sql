f0b1850dad2607d21e62269bfcf8e995bde7dec5
unknown <yuraniester@gmail.com>
2023-12-06 03:56:20 -0500

    Arreglo base de datos 2

--- MySQL/BD_SchoolBoard.sql
+++ MySQL/BD_SchoolBoard.sql
@@ -23,46 +23,11 @@ CREATE TABLE usuario (
     PRIMARY KEY (documento)
 );
 
-INSERT INTO usuario (documento, clave, rol, estado, tipoDoc, nombres, apellidos, correo, foto) VALUES 
-(1, MD5('administrador'), 'Administrador', 'activo', 'CC','Administrador', 'Prueba', 'yuraniester@gmail.com', '../../Uploads/Usuario/fotoUsuario.jpg'),
-(2, MD5('docente'), 'Docente', 'activo', 'CC','Felipe', 'Restrepo','lfrestrepo004@gmail.com', '../../Uploads/Usuario/fotoUsuario.jpg'),
-(3, MD5('estudiante'), 'Estudiante', 'activo', 'TI','Estudiante', 'Prueba', 'yuraniester@gmail.com', '../../Uploads/Usuario/fotoUsuario.jpg'),
-
-(4, MD5('docente'), 'Docente', 'activo', 'CC','Docente', 'Prueba', 'yuraniester@gmail.com', '../../Uploads/Usuario/fotoUsuario.jpg'),
-(1023163093, MD5('docente'), 'Docente', 'activo', 'CC','Angelica Maria', 'Triana Guarnizo', 'angelica@gmail.com', '../../Uploads/Usuario/userAngelica.jpg'),
-
-(1023163094, MD5('estudiante'), 'Estudiante', 'activo', 'TI','Nicole Dayana', 'Benavides Alfonso', 'nicole.benavides@misena.edu.co', '../../Uploads/Usuario/userNicole.jpg'),
--- (1023163094, MD5('estudiante'), 'Estudiante', 'activo', 'TI','Nicole Dayana', 'Benavides Alfonso', NULL, '../../Uploads/Usuario/userNicole.jpg'),
-(1023163095, MD5('estudiante'), 'Estudiante', 'activo', 'TI','Favian Andres', 'Mancilla Angulo', 'favian@gmail.com', '../../Uploads/Usuario/userFavian.jpg'),
-(1023163096, MD5('estudiante'), 'Estudiante', 'activo', 'TI','Darwin', 'Urbina Lopez', 'darwin@gmail.com', '../../Uploads/Usuario/userDarwin.jpg'),
-
-(1023163097, MD5('estudiante'), 'Estudiante', 'activo', 'TI','Camila Dayana', 'Benavides Alfonso', 'Camila@gmail.com', '../../Uploads/Usuario/userCamila.jpg'),
-(1023163098, MD5('estudiante'), 'Estudiante', 'activo', 'TI','Camilo Andres', 'Mancilla Angulo', 'Camilo@gmail.com', '../../Uploads/Usuario/userCamilo.jpg'),
-(1023163099, MD5('estudiante'), 'Estudiante', 'activo', 'TI','Camilin', 'Urbina Lopez', 'Camilin@gmail.com', '../../Uploads/Usuario/userCamilin.jpg');
-
-
 CREATE TABLE asignatura (
     idAsignatura INT AUTO_INCREMENT,
     nombre VARCHAR(100),
     PRIMARY KEY (idAsignatura)
 );
-
-INSERT INTO asignatura (nombre) VALUES 
-('Matemáticas'),
-('Calculo'),
-
-('Sociales'),
-('Religión'),
-('Catedra para la paz'),
-
-('Ciencias'),
-('Quimica'),
-('Fisica'),                                                                                                                                                                         
-
-('Español'),
-('Ingles'),
-('Ed.Fisica'),
-('Informatica');
 
 CREATE TABLE curso (
     idCurso INT AUTO_INCREMENT,
@@ -71,26 +36,12 @@ CREATE TABLE curso (
     PRIMARY KEY (idCurso)
 );
 
-INSERT INTO curso (jornada, nombre) VALUES
-('', 'Todos') --No quitar este registro nunca
-('unica', 'SEXTO'),
-('unica', 'SEPTIMO'),
-('unica', 'OCTAVO'),
-('unica', 'NOVENO'),
-('unica', 'DECIMO'),
-('unica', 'ONCE');
-
 CREATE TABLE aula (
     idAula INT AUTO_INCREMENT,
     nombre VARCHAR(50),
     capacidad VARCHAR(3),
     PRIMARY KEY (idAula)
 );
-
-INSERT INTO aula(nombre, capacidad) VALUES 
-('119 Torre oriental', '20'),
-('101  Festo', '30'),
-('301 Torre occidental', '25');
 
 CREATE TABLE observador (
     idObservador INT AUTO_INCREMENT,
@@ -102,11 +53,6 @@ CREATE TABLE observador (
     FOREIGN KEY (idEstudiante) REFERENCES usuario(documento),
     FOREIGN KEY (idAutor) REFERENCES usuario(documento)
 );
-
-INSERT INTO observador(idEstudiante, idAutor, observacion) VALUES 
-(3, 1,'El estudiante no cumple con el uniforme de la institución'),
-(3, 1,'El estudiante no presta atención en clase y distrae a sus compañeros'),
-(1023163094, 2,'El estudiante, ha fallado 3 veces seguidas sin presentar excusa.');
 
 
 CREATE TABLE comunicado (
@@ -122,13 +68,6 @@ CREATE TABLE comunicado (
     FOREIGN KEY (idCurso) REFERENCES curso(idCurso)
 );
 
-INSERT INTO comunicado(idUsuario, idCurso, fecha, titulo, descripcion, archivos) VALUES
-(1023163093, 1, '2023-11-26 20:21:51', 'Innovación y Crecimiento', 'Estimada comunidad educativa, Nos complace compartir una emocionante noticia: el lanzamiento de nuestro nuevo programa, "Innovación y Crecimiento". Este proyecto refleja nuestro compromiso continuo con la excelencia académica y el desarrollo integral de nuestros estudiantes. Estamos emocionados por incorporar nuevas tecnologías y actividades que promuevan habilidades esenciales para el siglo XXI.','../Vista/Uploads/Comunicados/Arte Marcial - Felipe Angarita.pdf'),
-
-
-(2, 2, NOW(), 'Cancelación de clase', 'El día de mañana no habrá clase en porque se suspenderá el agua en el colegio','../Vista/Uploads/Comunicados/Arte Marcial - Felipe Angarita.pdf'),
-(1, 1, NOW(), 'Entrega de boletines', 'La entrega de voletines se hará el día 20/11/2024 desde las 8:00a.m. En los proximos días el director de cada uno de los cursos estará comunicando la hora exacta de la reunión para cada estudiante.','../Vista/Uploads/Comunicados/Arte Marcial - Felipe Angarita.pdf');
-
 CREATE TABLE clase (
     idClase INT AUTO_INCREMENT,
     idCurso INT,
@@ -143,17 +82,6 @@ CREATE TABLE clase (
     FOREIGN KEY (idAula) REFERENCES aula(idAula)
 );
 
-INSERT INTO clase (idCurso, idAsignatura, idDocente, idAula) VALUES
--- Sexto, - Matematicas - Felipe - 119 
-(2, 1, 2, 1),
--- Sexto, - Sociales - Angelica - festo 
-(2, 3, 1023163093, 2),
--- Septimo - Matematicas - Felipe - 119
-(3 , 1, 2, 1),
-(3, 1, 2, 1),
-(4, 1, 2, 1),
-(4, 7, 1023163093, 2);
-
 CREATE TABLE acudiente (
     documento INT,
     nombres VARCHAR(100),
@@ -163,11 +91,6 @@ CREATE TABLE acudiente (
     PRIMARY KEY (documento)
 );
 
-INSERT INTO acudiente (documento, nombres, apellidos, telefono, correo) VALUES
-(1024163098, 'Lilia', 'Alfonso', 3102274510, 'yuraniester@gmail.com');
-(1024163011, 'Camila', 'Rodriguez', 3104544510, 'nicole.benavides@misena.edu.co');
-(1015213099, 'Steven', 'Blanco', 3102342510, 'yuraniester@gmail.com');
-
 CREATE TABLE estudianteAcudiente (
     idEstudianteAcudiente INT AUTO_INCREMENT,
     idAcudiente INT,
@@ -176,11 +99,6 @@ CREATE TABLE estudianteAcudiente (
     FOREIGN KEY (idAcudiente) REFERENCES acudiente(documento),
     FOREIGN KEY (idEstudiante) REFERENCES usuario(documento)
 );
-
-INSERT INTO estudianteAcudiente (idAcudiente, idEstudiante) VALUES
-(1024163098, 1023163094);
-(1024163098, 1023163096);
-(1024163098, 1023163095);
 
 CREATE TABLE estudianteCurso (
     idestudianteCurso INT AUTO_INCREMENT,
@@ -192,14 +110,6 @@ CREATE TABLE estudianteCurso (
     FOREIGN KEY (idEstudiante) REFERENCES usuario(documento)
 );
 
-INSERT INTO estudianteCurso (idCurso, idEstudiante) VALUES 
-(2, 1023163094),
-(2, 1023163095),
-(2, 1023163096),
-(3, 1023163097),
-(3, 1023163098),
-(3, 1023163099);
-
 CREATE TABLE asistencia (
     idAsistencia INT AUTO_INCREMENT,
     idClase INT,
@@ -210,11 +120,6 @@ CREATE TABLE asistencia (
     FOREIGN KEY (idClase) REFERENCES clase(idClase),
     FOREIGN KEY (idEstudiante) REFERENCES usuario(documento)
 );
-
-INSERT INTO asistencia (idAsistencia,idClase, idEstudiante, fecha, estado) VALUES
-(1, 1, 3,'2023-09-05 13:12:13','Asiste'),
-(2, 1, 3,'2023-09-05 13:12:13', 'Falta'),
-(3, 1, 3,'2023-09-05 13:12:13', 'Retardo');
 
 
 CREATE TABLE tarea (
@@ -231,14 +136,6 @@ CREATE TABLE tarea (
     FOREIGN KEY (idDocente) REFERENCES usuario(documento)
 );
 
-INSERT INTO tarea (idClase,idDocente, titulo, descripcion, fecha_creacion, fecha_vencimiento, archivos) VALUES
-(1, 2, 'Ejercicios Matemáticas', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus magnam enim natus explicabo amet beatae saepe iste veniam sed quisquam.', '2023-09-05 13:12:13', '2023-09-13 13:11:05', 'CausanRaiznJR___7164e8ec1f892a8___%20(1).pdf'),
-(2, 2, 'Ensayo sobre Tecnologia', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus magnam enim natus explicabo amet beatae saepe iste veniam sed quisquam.', '2023-09-05 13:12:13', '2023-09-24 00:48:12', NULL),
-(1, 2, 'Algoritmia', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus magnam enim natus explicabo amet beatae saepe iste veniam sed quisquam.', '2023-09-05 13:12:13', '2023-05-08 13:11:05', NULL),
-(1, 2, 'Matematicas', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus magnam enim natus explicabo amet beatae saepe iste veniam sed quisquam.', '2023-09-05 13:12:13', '2023-05-08 13:11:05', NULL);
-
-
-
 CREATE TABLE entrega (
     idEntrega INT AUTO_INCREMENT,
     idEstudiante INT,
@@ -251,12 +148,6 @@ CREATE TABLE entrega (
     FOREIGN KEY (idTarea) REFERENCES tarea(idTarea)
 );
 
-INSERT INTO entrega (descripcion, archivos, idEstudiante, idTarea) VALUES 
-('Entrega Nicole', NULL, 1023163094, 1),
-('Entrega Erika', NULL, 1023163095, 1),
-('Entrega Tatiana', NULL, 1023163096, 1);
-
-
 CREATE TABLE calificacion(
     idCalificacion INT AUTO_INCREMENT,
     idEntrega INT ,
@@ -267,6 +158,97 @@ CREATE TABLE calificacion(
     FOREIGN KEY (idEntrega) REFERENCES entrega(idEntrega)
 );
 
+INSERT INTO usuario (documento, clave, rol, estado, tipoDoc, nombres, apellidos, correo, foto) VALUES 
+(1, MD5('administrador'), 'Administrador', 'activo', 'CC','Administrador', 'Prueba', 'yuraniester@gmail.com', '../../Uploads/Usuario/fotoUsuario.jpg'),
+(2, MD5('docente'), 'Docente', 'activo', 'CC','Felipe', 'Restrepo','lfrestrepo004@gmail.com', '../../Uploads/Usuario/userFelipe.jpg'),
+(3, MD5('estudiante'), 'Estudiante', 'activo', 'TI','Estudiante', 'Prueba', 'yuraniester@gmail.com', '../../Uploads/Usuario/fotoUsuario.jpg'),
+
+(4, MD5('docente'), 'Docente', 'activo', 'CC','Docente', 'Prueba', 'yuraniester@gmail.com', '../../Uploads/Usuario/fotoUsuario.jpg'),
+(1023163093, MD5('docente'), 'Docente', 'activo', 'CC','Angelica Maria', 'Triana Guarnizo', 'angelica@gmail.com', '../../Uploads/Usuario/userAngelica.jpg'),
+
+(1023163094, MD5('estudiante'), 'Estudiante', 'activo', 'TI','Nicole Dayana', 'Benavides Alfonso', 'nicole.benavides@misena.edu.co', '../../Uploads/Usuario/userNicole.jpg'),
+-- (1023163094, MD5('estudiante'), 'Estudiante', 'activo', 'TI','Nicole Dayana', 'Benavides Alfonso', NULL, '../../Uploads/Usuario/userNicole.jpg'),
+(1023163095, MD5('estudiante'), 'Estudiante', 'activo', 'TI','Favian Andres', 'Mancilla Angulo', 'favian@gmail.com', '../../Uploads/Usuario/userFavian.jpg'),
+(1023163096, MD5('estudiante'), 'Estudiante', 'activo', 'TI','Darwin', 'Urbina Lopez', 'darwin@gmail.com', '../../Uploads/Usuario/userDarwin.jpg'),
+
+(1023163097, MD5('estudiante'), 'Estudiante', 'activo', 'TI','Camila Dayana', 'Benavides Alfonso', 'Camila@gmail.com', '../../Uploads/Usuario/fotoUsuario.jpg'),
+(1023163098, MD5('estudiante'), 'Estudiante', 'activo', 'TI','Camilo Andres', 'Mancilla Angulo', 'Camilo@gmail.com', '../../Uploads/Usuario/fotoUsuario.jpg'),
+(1023163099, MD5('estudiante'), 'Estudiante', 'activo', 'TI','Camilin', 'Urbina Lopez', 'Camilin@gmail.com', '../../Uploads/Usuario/fotoUsuario.jpg');
+
+INSERT INTO asignatura (nombre) VALUES 
+('Matemáticas'),
+('Quimica'),
+('Fisica'),
+('Español'),
+('Ed.Fisica'),
+('Informatica');
+
+INSERT INTO curso (jornada, nombre) VALUES
+('', 'Todos'),
+('unica', 'SEXTO'),
+('unica', 'SEPTIMO'),
+('unica', 'OCTAVO'),
+('unica', 'NOVENO'),
+('unica', 'DECIMO'),
+('unica', 'ONCE');
+
+INSERT INTO aula(nombre, capacidad) VALUES 
+('119 Torre oriental', '20'),
+('101  Festo', '30'),
+('301 Torre occidental', '25');
+
+INSERT INTO observador(idEstudiante, idAutor, observacion) VALUES 
+(3, 1,'El estudiante no cumple con el uniforme de la institución'),
+(3, 1,'El estudiante no presta atención en clase y distrae a sus compañeros'),
+(1023163094, 2,'El estudiante, ha fallado 3 veces seguidas sin presentar excusa.');
+
+INSERT INTO comunicado(idUsuario, idCurso, fecha, titulo, descripcion, archivos) VALUES
+(1023163093, 1, '2023-11-26 20:21:51', 'Innovación y Crecimiento', 'Estimada comunidad educativa, Nos complace compartir una emocionante noticia: el lanzamiento de nuestro nuevo programa, "Innovación y Crecimiento". Este proyecto refleja nuestro compromiso continuo con la excelencia académica y el desarrollo integral de nuestros estudiantes. Estamos emocionados por incorporar nuevas tecnologías y actividades que promuevan habilidades esenciales para el siglo XXI.','../Vista/Uploads/Comunicados/Arte Marcial - Felipe Angarita.pdf'),
+(2, 2, NOW(), 'Cancelación de clase', 'El día de mañana no habrá clase en porque se suspenderá el agua en el colegio','../Vista/Uploads/Comunicados/Arte Marcial - Felipe Angarita.pdf'),
+(1, 1, NOW(), 'Entrega de boletines', 'La entrega de voletines se hará el día 20/11/2024 desde las 8:00a.m. En los proximos días el director de cada uno de los cursos estará comunicando la hora exacta de la reunión para cada estudiante.','../Vista/Uploads/Comunicados/Arte Marcial - Felipe Angarita.pdf');
+
+INSERT INTO acudiente (documento, nombres, apellidos, telefono, correo) VALUES
+(1023163098, 'Lilia', 'Alfonso', '3102274510', 'yuraniester@gmail.com'),
+(1024163011, 'Camila', 'Rodriguez', '3104544510', 'nicole.benavides@misena.edu.co'),
+(1015213099, 'Steven', 'Blanco', '3102342510', 'yuraniester@gmail.com');
+
+INSERT INTO estudianteAcudiente (idAcudiente, idEstudiante) VALUES
+(1023163098, 1023163094),
+(1024163011, 1023163096),
+(1015213099, 1023163095);
+
+INSERT INTO estudianteCurso (idCurso, idEstudiante) VALUES 
+(2, 3),
+(2, 1023163094),
+(2, 1023163095),
+(2, 1023163096),
+(3, 1023163097),
+(3, 1023163098),
+(3, 1023163099);
+
+INSERT INTO clase (idCurso, idAsignatura, idDocente, idAula) VALUES
+(2, 1, 2, 1),
+(2, 3, 1023163093, 2),
+(3 , 1, 2, 1),
+(3, 1, 2, 1),
+(4, 1, 2, 1),
+(4, 2, 1023163093, 2);
+
+INSERT INTO asistencia (idClase, idEstudiante, fecha, estado) VALUES
+(1, 1023163094,'2023-09-05 13:12:13','Asiste'),
+(1, 1023163094,'2023-09-05 13:12:13', 'Falta'),
+(1, 1023163094,'2023-09-05 13:12:13', 'Retardo');
+
+INSERT INTO tarea (idClase,idDocente, titulo, descripcion, fecha_creacion, fecha_vencimiento, archivos) VALUES
+(1, 2, 'Ejercicios Matemáticas', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus magnam enim natus explicabo amet beatae saepe iste veniam sed quisquam.', '2023-09-05 13:12:13', '2023-09-13 13:11:05', 'CausanRaiznJR___7164e8ec1f892a8___%20(1).pdf'),
+(2, 2, 'Ensayo sobre Tecnologia', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus magnam enim natus explicabo amet beatae saepe iste veniam sed quisquam.', '2023-09-05 13:12:13', '2023-09-24 00:48:12', NULL),
+(1, 2, 'Algoritmia', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus magnam enim natus explicabo amet beatae saepe iste veniam sed quisquam.', '2023-09-05 13:12:13', '2023-05-08 13:11:05', NULL),
+(1, 2, 'Matematicas', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus magnam enim natus explicabo amet beatae saepe iste veniam sed quisquam.', '2023-09-05 13:12:13', '2023-05-08 13:11:05', NULL);
+
+INSERT INTO entrega (descripcion, archivos, idEstudiante, idTarea) VALUES 
+('Entrega Nicole', NULL, 1023163094, 1),
+('Entrega Erika', NULL, 1023163095, 1),
+('Entrega Tatiana', NULL, 1023163096, 1);
 
 INSERT INTO calificacion (idEntrega, nota) VALUES 
 (1, '4.5');
